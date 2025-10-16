f2f02d5ae272a2b45fb3315430793984f241a9f4
Eduardo <eduardovcn74@gmail.com>
2025-10-16 20:28:24 -0300

    Criando o arquivo banco, para armazenar os dados em tempo de execução.

---
+++ ContaSystem/src/contas/Banco.java
@@ -0,0 +1,39 @@
+package contas;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+    // usando o final para garantir que a referência da lista não será alterada indevidamente.
+    //A lista é inicializada como uma nova ArrayList, garantindo que o objeto lista esteja sempre pronto para uso.
+    private static final List<Conta> contasCorrente = new ArrayList<>();
+
+    // Método para adicionar uma nova contaCorrente ao banco
+    public static void adicionarConta(Conta conta) {
+        contasCorrente.add(conta);
+    }
+
+    // Método para imprimir todas as contasCorrente no banco. Só para ver se está funcionando. Depois remover.
+    public static void imprimirContas() {
+        for (Conta conta : contasCorrente) {
+            System.out.println(conta);
+
+        }
+    }
+
+    public static Conta getConta(int numeroConta) {
+        for (Conta conta : contasCorrente) {
+            if (conta.getNumeroConta() == numeroConta) {
+                System.out.println("\nNome: " + conta.getNome());
+                System.out.printf("");
+                System.out.println("Saldo: " + conta.getSaldo());
+            }
+            else {
+                System.out.println("\nConta não encontrada.");
+            }
+        }
+
+        return null;
+    }
+
+    // Outros métodos para listar, remover, etc.
+}
